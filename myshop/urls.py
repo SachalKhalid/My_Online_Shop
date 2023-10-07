3edf8b66121754ced26c96d26c1fa4dc83170e6f
SachalKhalid <bcsf20a029@pucit.edu.pk>
2023-10-07 20:14:14 +0500

    Added Orders Funtionality

--- myshop/urls.py
+++ myshop/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('shop/', include('shop.urls', namespace='shop')),
+
 ]
 
 if settings.DEBUG:
